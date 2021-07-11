9c921a6be4b52811bda58c8ea8df371df14bcd6b
Mario Cardoza <mariokr.rocker@gmail.com>
2021-07-11 01:42:59 -0600

    Update SaleController.php

--- app/Http/Controllers/SaleController.php
+++ app/Http/Controllers/SaleController.php
@@ -134,7 +134,7 @@ class SaleController extends Controller
             $product_detail->product_id = $product->id;
             $product_detail->table = 'Product';
             $product_detail->sale_id = $sale_id;
-            $product_detail->type =2;
+            $product_detail->type =0;
             $product_detail->state =0;
             $product_detail->save();
         }
